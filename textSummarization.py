477b4ef54d8a11234747f90245469bad8702f3b4
ordaz00 <ordazjose00@gmail.com>
2023-04-28 19:39:02 -0700

    fixed some issues with the google cloud uploading, and put processes in functions

--- textSummarization.py
+++ textSummarization.py
@@ -3,14 +3,38 @@ from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 from pyspark.ml.feature import HashingTF, IDF
 from google.cloud import storage
+import os
+
+# Correcting the python versions
+os.environ["PYSPARK_PYTHON"] = "/Users/ordaz00/miniconda3/envs/pyspark_env/bin/python"
+os.environ["PYSPARK_DRIVER_PYTHON"] = "/Users/ordaz00/miniconda3/envs/pyspark_env/bin/python"
+
 
 # Initialize the spark session
 spark = SparkSession.builder \
     .appName("Text Summarization") \
+    .config("spark.executor.memory", "14g") \
+    .config("spark.driver.memory", "14g") \
+    .config("spark.cleaner.referenceTracking.cleanCheckpoints", "true") \
+    .config("spark.cleaner.referenceTracking.blocking", "true") \
+    .config("spark.cleaner.referenceTracking.blockingFraction", "0.5") \
     .getOrCreate()
 
 # Initializing storage client
 storage_client = storage.Client.from_service_account_json('ornate-woodland-384921-a68699295d78.json')
+
+# Column names
+text_columns = ["article", "highlights"]
+clean_text_columns = ["clean_article", "clean_highlights"]
+tokens_columns = ["tokens_article", "tokens_highlights"]
+raw_features_columns = ["raw_features_article", "raw_features_highlights"]
+features_columns = ["features_article", "features_highlights"]
+
+# Bucket Variables
+bucket_name = "dailynews_text_summarization"
+train_blob_name = "preprocessed_training.json"
+test_blob_name = "preprocessed_test.json"
+validation_blob_name = "preprocessed_validation.json"
 
 # Preprocess text
 def preprocess_text(data, text_columns, clean_text_columns, tokens_columns):
@@ -49,35 +73,28 @@ def upload_json_string(bucket_name, json_data, destination_blob_name):
     # Upload the JSON string to Google Cloud Storage
     blob.upload_from_string(json_string, content_type='application/json')
 
-# Load the datasets
-train_data = spark.read.csv("train.csv", header=True, inferSchema=True)
-test_data = spark.read.csv("test.csv", header=True, inferSchema=True)
-validation_data = spark.read.csv("validation.csv", header=True, inferSchema=True)
+def prepareDatasets():
+    # Load the datasets
+    train_data = spark.read.csv("train.csv", header=True, inferSchema=True)
+    test_data = spark.read.csv("test.csv", header=True, inferSchema=True)
+    validation_data = spark.read.csv("validation.csv", header=True, inferSchema=True)
 
-# Column names
-text_columns = ["article", "highlights"]
-clean_text_columns = ["clean_article", "clean_highlights"]
-tokens_columns = ["tokens_article", "tokens_highlights"]
-raw_features_columns = ["raw_features_article", "raw_features_highlights"]
-features_columns = ["features_article", "features_highlights"]
+    # Repartition the datasets
+    train_data = train_data.repartition(200)
+    test_data = test_data.repartition(200)
+    validation_data = validation_data.repartition(200)
 
-# Bucket Variables
-bucket_name = "dailynews_text_summarization"
-train_blob_name = "preprocessed_training.json"
-test_blob_name = "preprocessed_test.json"
-validation_blob_name = "preprocessed_validation.json"
+    # Process the datasets
+    train_data = process_data(train_data, text_columns, clean_text_columns, tokens_columns, raw_features_columns, features_columns)
+    test_data = process_data(test_data, text_columns, clean_text_columns, tokens_columns, raw_features_columns, features_columns)
+    validation_data = process_data(validation_data, text_columns, clean_text_columns, tokens_columns, raw_features_columns, features_columns)
 
-# Process the datasets
-train_data = process_data(train_data, text_columns, clean_text_columns, tokens_columns, raw_features_columns, features_columns)
-test_data = process_data(test_data, text_columns, clean_text_columns, tokens_columns, raw_features_columns, features_columns)
-validation_data = process_data(validation_data, text_columns, clean_text_columns, tokens_columns, raw_features_columns, features_columns)
-
-# Convert dataframes to json
-train_data_json = train_data.toJSON().collect()
-test_data_json = test_data.toJSON().collect()
-validation_data_json = validation_data.toJSON().collect()
-
-# Upload the JSON strings to Google Cloud Storage
-upload_json_string(bucket_name, train_data_json, train_blob_name)
-upload_json_string(bucket_name, test_data_json, test_blob_name)
-upload_json_string(bucket_name, validation_data_json, validation_blob_name)
+def uploadToCloud(train_data_json, test_data_json, validation_data_json):
+    # Convert dataframes to json
+    train_data_json = train_data.toJSON().saveAsTextFile("train_data_preprocessed")
+    test_data_json = test_data.toJSON().collect()
+    validation_data_json = validation_data.toJSON().collect()
+    # Upload the JSON strings to Google Cloud Storage
+    upload_json_string(bucket_name, "train_data_preprocessed", train_blob_name)
+    upload_json_string(bucket_name, test_data_json, test_blob_name)
+    upload_json_string(bucket_name, validation_data_json, validation_blob_name)
