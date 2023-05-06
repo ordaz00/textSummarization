b387afdff74d6bd0a1825c2b857bd45880d382b4
ordaz00 <ordazjose00@gmail.com>
2023-05-06 14:08:00 -0700

    added validation and testing data, to validate and test the model

--- textSummarization.py
+++ textSummarization.py
@@ -26,9 +26,31 @@ train_data_json = train_data_blob.download_as_text()
 train_data_list = [json.loads(line) for line in train_data_json.split('\n') if line]
 train_data = spark.createDataFrame(train_data_list)
 
+# Load validation data from Google Cloud Storage as a DataFrame
+validation_blob_name = "preprocessed_validation.json"
+validation_data_blob = bucket.blob(validation_blob_name)
+validation_data_json = validation_data_blob.download_as_text()
+validation_data_list = [json.loads(line) for line in validation_data_json.split('\n') if line]
+validation_data = spark.createDataFrame(validation_data_list)
+
+# Load test data from Google Cloud Storage as a DataFrame
+test_blob_name = "preprocessed_test.json"
+test_data_blob = bucket.blob(test_blob_name)
+test_data_json = test_data_blob.download_as_text()
+test_data_list = [json.loads(line) for line in test_data_json.split('\n') if line]
+test_data = spark.createDataFrame(test_data_list)
+
 # Assuming 'features_article' is the input_seq and 'features_highlights' is the target_seq
 input_sequences = train_data.select("features_article").rdd.map(lambda x: np.array(x[0])).collect()
 target_sequences = train_data.select("features_highlights").rdd.map(lambda x: np.array(x[0])).collect()
+
+# Create input and target sequences for the validation dataset
+validation_input_sequences = validation_data.select("features_article").rdd.map(lambda x: np.array(x[0])).collect()
+validation_target_sequences = validation_data.select("features_highlights").rdd.map(lambda x: np.array(x[0])).collect()
+
+# Create input and target sequences for the test dataset
+test_input_sequences = test_data.select("features_article").rdd.map(lambda x: np.array(x[0])).collect()
+test_target_sequences = test_data.select("features_highlights").rdd.map(lambda x: np.array(x[0])).collect()
 
 # Simple Seq2Seq model using TensorFlow
 encoder_inputs = tf.keras.Input(shape=(None, input_sequences[0].shape[0]))
@@ -47,3 +69,11 @@ model = tf.keras.Model([encoder_inputs, decoder_inputs], decoder_outputs)
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 model.fit([input_sequences, target_sequences], target_sequences, batch_size=64, epochs=100)
+
+# Evaluate the model on validation dataset
+validation_loss, validation_accuracy = model.evaluate([validation_input_sequences, validation_target_sequences], validation_target_sequences, batch_size=64)
+print(f'Validation loss: {validation_loss}, Validation accuracy: {validation_accuracy}')
+
+# Evaluate the model on test dataset
+test_loss, test_accuracy = model.evaluate([test_input_sequences, test_target_sequences], test_target_sequences, batch_size=64)
+print(f'Test loss: {test_loss}, Test accuracy: {test_accuracy}')
