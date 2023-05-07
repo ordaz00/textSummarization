4a06b5b3469ebc227ed3861a7d5458025ca73c7c
ordaz00 <ordazjose00@gmail.com>
2023-05-06 18:48:31 -0700

    added a shell script to avoid having to type all the run commands by hand

---
+++ run.sh
@@ -0,0 +1,14 @@
+# Build Docker Image
+docker build -t your_image_name .
+
+# Tag Docker Image
+docker tag text_summarization gcr.io/ornate-woodland-384921/text_summarization
+
+# Push Docker Image
+docker push gcr.io/ornate-woodland-384921/text_summarization
+
+# Run Docker Container
+gcloud ai custom-jobs create \
+  --region=us-central1 \
+  --display-name=text_summarization2 \
+  --worker-pool-spec=machine-type=n1-standard-4,replica-count=1,container-image-uri=gcr.io/ornate-woodland-384921/text_summarization
