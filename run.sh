095a809dbeed5f91619f68baffea511517999a9f
ordaz00 <ordazjose00@gmail.com>
2023-05-07 21:49:27 -0700

    made minor changes to the run.sh file

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ docker push gcr.io/ornate-woodland-384921/text_summarization
 # Run Docker Container
 gcloud ai custom-jobs create \
   --region=us-central1 \
-  --display-name=text_summarization2 \
+  --display-name=text_summarization4 \
   --worker-pool-spec=machine-type=n1-standard-4,replica-count=1,container-image-uri=gcr.io/ornate-woodland-384921/text_summarization
 
 # Print updates from the cloud
